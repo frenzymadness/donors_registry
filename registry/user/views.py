dc22d4d5f1c281686cd212d490afcdd356e3522b
Lumir Balhar <lbalhar@redhat.com>
2020-09-07 06:18:48 +0200

    Remove unused useless members view and template

--- registry/user/views.py
+++ registry/user/views.py
@@ -1,12 +1,4 @@
 """User views."""
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint
 
 blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
-
-
-@blueprint.route("/")
-@login_required
-def members():
-    """List members."""
-    return render_template("users/members.html")
