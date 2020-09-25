b0496a4105cf48169404d48f4711a715940e4ef7
Štěpán Tomsa <stomsa@redhat.com>
2020-09-25 17:33:28 +0200

    Přidej modely číselníkům odměn
    Přidány modely číselníku ocenění. Přidána vazba uděleným oceněním.
    Dárci se přiřazují podle rodného čísla, ale nemůže to být klíč.
    Neevidujeme dárce jako samostatné záznamy.

---
+++ migrations/versions/9d51ae0ab1e0_create_medals_tables.py
@@ -0,0 +1,48 @@
+"""create_medals_tables
+
+Revision ID: 9d51ae0ab1e0
+Revises: 149e19e8c994
+Create Date: 2020-09-25 17:30:04.933587
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "9d51ae0ab1e0"
+down_revision = "149e19e8c994"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "medals",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("slug", sa.String(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("slug"),
+    )
+    op.create_table(
+        "awarded_medals",
+        sa.Column("rodne_cislo", sa.String(length=10), nullable=False),
+        sa.Column("medal", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["medal"],
+            ["medals.id"],
+        ),
+        sa.PrimaryKeyConstraint("rodne_cislo", "medal"),
+    )
+    op.create_index(
+        op.f("ix_awarded_medals_rodne_cislo"),
+        "awarded_medals",
+        ["rodne_cislo"],
+        unique=False,
+    )
+
+
+def downgrade():
+    op.drop_index(op.f("ix_awarded_medals_rodne_cislo"), table_name="awarded_medals")
+    op.drop_table("awarded_medals")
+    op.drop_table("medals")
