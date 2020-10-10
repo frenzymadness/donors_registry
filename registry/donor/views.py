06383dabc52d3715af7e76f053ea9acec5c97554
Lumir Balhar <lbalhar@redhat.com>
2020-10-10 15:33:32 +0200

    Split functions with GET/POST functionalities to two functions

--- registry/donor/views.py
+++ registry/donor/views.py
@@ -12,28 +12,31 @@ from .models import Batch, Record
 blueprint = Blueprint("donor", __name__, static_folder="../static")
 
 
-@blueprint.route("/import/", methods=["GET", "POST"])
+@blueprint.route("/import/", methods=("GET",))
 @login_required
 def import_data():
-    if request.method == "GET":
-        import_form = ImportForm()
-        return render_template("donor/import.html", form=import_form)
-    elif request.method == "POST":
-        import_form = ImportForm(request.form)
-        if import_form.validate_on_submit():
-            batch = Batch(
-                donation_center=import_form.donation_center.id,
-                imported_at=datetime.now(),
-            )
-            db.session.add(batch)
-            db.session.commit()
+    import_form = ImportForm()
+    return render_template("donor/import.html", form=import_form)
 
-            for line in import_form.valid_lines_content:
-                record = Record.from_list([batch.id] + line.split(";"))
-                db.session.add(record)
-            db.session.commit()
-            flash("Import proběhl úspěšně")
-            return redirect(url_for("public.home"))
-        else:
-            flash_errors(import_form)
-            return render_template("donor/import.html", form=import_form)
+
+@blueprint.route("/import/", methods=("POST",))
+@login_required
+def import_data_post():
+    import_form = ImportForm(request.form)
+    if import_form.validate_on_submit():
+        batch = Batch(
+            donation_center=import_form.donation_center.id,
+            imported_at=datetime.now(),
+        )
+        db.session.add(batch)
+        db.session.commit()
+
+        for line in import_form.valid_lines_content:
+            record = Record.from_list([batch.id] + line.split(";"))
+            db.session.add(record)
+        db.session.commit()
+        flash("Import proběhl úspěšně")
+        return redirect(url_for("public.home"))
+    else:
+        flash_errors(import_form)
+        return render_template("donor/import.html", form=import_form)
