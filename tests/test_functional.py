9d6fe328ba69fa05b01450487fdc15559dd19109
Lumir Balhar <lbalhar@redhat.com>
2020-10-11 09:11:38 +0200

    Add two new tests for data import

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -61,3 +61,60 @@ class TestImport:
 
         assert Record.query.count() == existing_records + new_records
         assert Batch.query.count() == existing_batches + 1
+
+    def test_repairable_input(self, user, testapp):
+        """Tests an input file the import machinery should be able
+        repair automatically without any manual assistance from user"""
+        input_data = Path("tests/data/repairable_import.txt").read_text()
+        new_records = 12  # 15 lines in file - 2 empty lines - 1 without free donations
+        existing_records = Record.query.count()
+        existing_batches = Batch.query.count()
+
+        login(user, testapp)
+        res = testapp.get("/import/")
+        form = res.forms["importForm"]
+        form["input_data"] = input_data
+        res = form.submit()
+        assert res.status_code == 200
+        form = res.form
+        # There is one valid line
+        assert len(form["valid_lines"].value.splitlines()) == 1
+        # And the rest are invalid lines
+        assert len(form["invalid_lines"].value.splitlines()) == new_records - 1
+        # We have to have as many lines of errors as invalid lines
+        assert len(form["invalid_lines_errors"].value.splitlines()) == len(
+            form["invalid_lines"].value.splitlines()
+        )
+        # But everything should be fixed by the app so we should be
+        # ready to just submit the form again and see a sucessful import
+        res = form.submit().follow()
+        assert "Import proběhl úspěšně" in res
+        assert res.status_code == 200
+        assert Record.query.count() == existing_records + new_records
+        assert Batch.query.count() == existing_batches + 1
+
+    def test_invalid_input(self, user, testapp):
+        """Tests an invalid import the app cannot fix automaticaly"""
+        input_data = Path("tests/data/invalid_import.txt").read_text()
+        existing_records = Record.query.count()
+        existing_batches = Batch.query.count()
+        login(user, testapp)
+        res = testapp.get("/import/")
+        form = res.forms["importForm"]
+        form["input_data"] = input_data
+        # No matter how many times we submit the form because it contains
+        # invalid records so they never be imported
+        for _ in range(5):
+            res = form.submit()
+            assert res.status_code == 200
+            form = res.form
+            assert len(form["valid_lines"].value.splitlines()) == 0
+            # And the rest are invalid lines
+            assert len(form["invalid_lines"].value.splitlines()) == 5
+            # We have to have as many lines of errors as invalid lines
+            assert len(form["invalid_lines_errors"].value.splitlines()) == len(
+                form["invalid_lines"].value.splitlines()
+            )
+            assert "Import proběhl úspěšně" not in res
+        assert Record.query.count() == existing_records
+        assert Batch.query.count() == existing_batches
