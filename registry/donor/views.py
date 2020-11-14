1ec2f8cb05a61f8ef5219af361de2cdb93fb61ec
Lumir Balhar <lbalhar@redhat.com>
2020-11-14 12:48:32 +0100

    Pridej prehledovou tabulku s API na backendu

--- registry/donor/views.py
+++ registry/donor/views.py
@@ -1,15 +1,46 @@
 from datetime import datetime
 
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from datatables import ColumnDT, DataTables
+from flask import (
+    Blueprint,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 from flask_login import login_required
 
 from registry.extensions import db
 from registry.utils import flash_errors
 
 from .forms import ImportForm
-from .models import Batch, Record
+from .models import Batch, DonorsOverview, Record
 
 blueprint = Blueprint("donor", __name__, static_folder="../static")
+
+# TODO: Find a better place where to store these names (db.Column custom attribute?)
+COLUMN_NAMES = {
+    "rodne_cislo": "Rodné číslo",
+    "first_name": "Jméno",
+    "last_name": "Příjmení",
+    "address": "Adresa",
+    "city": "Město",
+    "postal_code": "PSČ",
+    "kod_pojistovny": "Pojišťovna",
+    "donation_count_fm": "Darování - Nemocnice FM",
+    "donation_count_fm_bubenik": "Darování - Krevní centrum",
+    "donation_count_trinec": "Darování - Nemocnice Třinec",
+    "donation_count_manual": "Darování - Jinde",
+    "donation_count_total": "Darování Celkem",
+    "awarded_medal_br": "Medaile - Bronz",
+    "awarded_medal_st": "Medaile - Stříbro",
+    "awarded_medal_zl": "Medaile - Zlato",
+    "awarded_medal_kr3": "Medaile - Kříž III",
+    "awarded_medal_kr2": "Medaile - Kříž II",
+    "awarded_medal_kr1": "Medaile - Kříž I",
+}
 
 
 @blueprint.route("/import/", methods=("GET",))
@@ -40,3 +71,23 @@ def import_data_post():
     else:
         flash_errors(import_form)
         return render_template("donor/import.html", form=import_form)
+
+
+@blueprint.route("/overview/", methods=("GET",))
+@login_required
+def overview():
+    return render_template("donor/overview.html", column_names=COLUMN_NAMES)
+
+
+@blueprint.route("/overview/data", methods=("GET",))
+@login_required
+def overview_data():
+    """JSON end point for JS Datatable"""
+    columns = [
+        ColumnDT(getattr(DonorsOverview, column_name), mData=column_name)
+        for column_name in COLUMN_NAMES.keys()
+    ]
+    query = db.session.query().select_from(DonorsOverview)
+    params = request.args.to_dict()
+    row_table = DataTables(params, query, columns)
+    return jsonify(row_table.output_result())
