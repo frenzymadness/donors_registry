203fa6e80103e56ef9d5256b8d5a76f0ad6b783f
Lumir Balhar <lbalhar@redhat.com>
2021-01-17 10:14:41 +0100

    Context procesor pro všechny medaile do navigačního pruhu

--- registry/utils.py
+++ registry/utils.py
@@ -1,9 +1,20 @@
 """Helper utilities and decorators."""
 from flask import flash
 
+from registry.donor.models import Medals
+
 
 def flash_errors(form, category="warning"):
     """Flash all errors for a form."""
     for field, errors in form.errors.items():
         for error in errors:
             flash(f"{getattr(form, field).label.text} - {error}", category)
+
+
+def template_globals():
+    """
+    Injected into all templates
+     - all medals are needed for the nav bar
+    """
+    all_medals = Medals.query.all()
+    return dict(all_medals=all_medals)
