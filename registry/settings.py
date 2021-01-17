3975f5c004637bc39fe1b47ed0e9867ab6fe952e
Lumir Balhar <lbalhar@redhat.com>
2021-01-17 08:44:52 +0100

    V DEBUG režimu povol také výpis SQL dotazů do logu aplikace

--- registry/settings.py
+++ registry/settings.py
@@ -13,6 +13,7 @@ env.read_env()  # reads .env file first
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
 SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+SQLALCHEMY_ECHO = DEBUG
 SECRET_KEY = env.str("SECRET_KEY")
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
 DEBUG_TB_ENABLED = DEBUG
