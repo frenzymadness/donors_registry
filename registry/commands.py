06959711c251d1e7f93ec28daef982c7400d0008
Lumir Balhar <lbalhar@redhat.com>
2021-01-17 10:13:01 +0100

    Vylepšení CLI a sloučení refresh-overview do install-test-data
    Fixes: https://github.com/frenzymadness/donors_registry/issues/33

--- registry/commands.py
+++ registry/commands.py
@@ -1,18 +1,18 @@
 """Commands for CLI"""
 import click
-from flask import Flask
+from flask import current_app
+from flask.cli import with_appcontext
 
 from registry.donor.models import DonorsOverview
 from registry.extensions import db
 from registry.user.models import User
 from tests.utils import test_data_medals, test_data_records
 
-app = Flask(__name__)
 
-
-@app.cli.command("create-user")
+@click.command()
 @click.argument("email")
 @click.argument("password")
+@with_appcontext
 def create_user(email, password):
     user = User(email, password)
     user.active = True
@@ -20,12 +20,12 @@ def create_user(email, password):
     db.session.commit()
 
 
-@app.cli.command("install-test-data")
+@click.command()
+@with_appcontext
 def install_test_data():
+    # Turn off SQLAlchemy logging (produces thousands of SQL queries)
+    current_app.config["SQLALCHEMY_ECHO"] = False
+
     test_data_records(db)
     test_data_medals(db)
-
-
-@app.cli.command("refresh-overview")
-def refresh_overview():
     DonorsOverview.refresh_overview()
