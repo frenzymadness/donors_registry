6c955423a9d88ed14c8811fc4719ff88e6e63703
j-jzk <jjezek@protonmail.com>
2021-05-26 17:28:14 +0200

    přidán validátor PSC / rodných čísel

--- registry/utils.py
+++ registry/utils.py
@@ -1,5 +1,6 @@
 """Helper utilities and decorators."""
 from flask import flash
+from wtforms.validators import ValidationError
 
 from registry.donor.models import Medals
 
@@ -18,3 +19,41 @@ def template_globals():
     """
     all_medals = Medals.query.all()
     return dict(all_medals=all_medals)
+
+
+class NumericValidator:
+    """
+    A WTForms validator for validating strings that consist only of digits and are
+    exactly of a specified length.
+    """
+
+    def __init__(self, length, msg_numeric=None, msg_length=None):
+        """
+        :param int length: The exact length the field must have
+        :param str msg_numeric: An error message for when the field contains forbidden
+            characters
+        :param str msg_length: An error message for when the field doesn't have
+            the specified length
+        """
+        self.length = length
+
+        if msg_numeric is None:
+            msg_numeric = "Pole musí obsahovat pouze číslice"
+        if msg_length is None:
+            plural = "znaků"
+            if length == 1:
+                plural = "znak"
+            elif length <= 4:
+                plural = "znaky"
+
+            msg_length = f"Pole musí mít právě {length} {plural}"
+
+        self.msg_numeric = msg_numeric
+        self.msg_length = msg_length
+
+    def __call__(self, form, field):
+        if field.data:
+            if not field.data.isdigit():
+                raise ValidationError(self.msg_numeric)
+            if len(field.data) != self.length:
+                raise ValidationError(self.msg_length)
