8a1689794b41ba4546391efb64d7f6250f4e6626
j-jzk <jjezek@protonmail.com>
2021-05-27 09:25:17 +0200

    přidány testy pro validátor PSC a čísla pojištovny

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+
+
 def login(user, testapp):
     res = testapp.post(
         "/", params={"email": user.email, "password": user.test_password}
     ).follow()
     assert "Přihlášení proběhlo úspěšně" in res
+
+
+class TestingForm(FlaskForm):
+    field = StringField()
