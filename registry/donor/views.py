bded98836fb75d209def0470b50ccc0453dd353f
Lumir Balhar <lbalhar@redhat.com>
2021-06-28 20:52:27 +0200

    Vlastní implementace datatables backendu
    Fixes: https://github.com/frenzymadness/donors_registry/issues/67
    Fixes: https://github.com/frenzymadness/donors_registry/issues/93

--- registry/donor/views.py
+++ registry/donor/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-from datatables import ColumnDT, DataTables
 from flask import (
     Blueprint,
     abort,
@@ -37,19 +36,8 @@ COLUMN_NAMES = {
     "city": "Město",
     "postal_code": "PSČ",
     "kod_pojistovny": "Pojišťovna",
-    "donation_count_fm": "Darování - Nemocnice FM",
-    "donation_count_fm_bubenik": "Darování - Krevní centrum",
-    "donation_count_trinec": "Darování - Nemocnice Třinec",
-    "donation_count_mp": "Darování - Moje Plazma",
-    "donation_count_manual": "Darování - Jinde",
-    "donation_count_total": "Darování Celkem",
-    "awarded_medal_br": "Medaile - Bronz",
-    "awarded_medal_st": "Medaile - Stříbro",
-    "awarded_medal_zl": "Medaile - Zlato",
-    "awarded_medal_kr3": "Medaile - Kříž III",
-    "awarded_medal_kr2": "Medaile - Kříž II",
-    "awarded_medal_kr1": "Medaile - Kříž I",
-    "awarded_medal_plk": "Plaketa ČČK",
+    "donations": "Darování Celkem",
+    "last_award": "Ocenění",
 }
 
 
@@ -59,18 +47,95 @@ def overview():
     return render_template("donor/overview.html", column_names=COLUMN_NAMES)
 
 
-@blueprint.get("/overview/data")
+@blueprint.get("/overview/data")  # noqa: C901 FIXME
 @login_required
 def overview_data():
     """JSON end point for JS Datatable"""
-    columns = [
-        ColumnDT(getattr(DonorsOverview, column_name), mData=column_name)
-        for column_name in COLUMN_NAMES.keys()
-    ]
-    query = db.session.query().select_from(DonorsOverview)
     params = request.args.to_dict()
-    row_table = DataTables(params, query, columns)
-    return jsonify(row_table.output_result())
+
+    # Lists and total numbers
+    all_medals = Medals.query.order_by(Medals.id.desc()).all()
+    all_donation_centers = DonationCenter.query.all()
+    all_records_count = DonorsOverview.query.count()
+    # WHERE part
+    if params["search[value]"]:
+        conditions_all = []
+        for part in params["search[value]"].split():
+            conditions_all.append([])
+            for column_name in COLUMN_NAMES.keys():
+                if hasattr(DonorsOverview, column_name):
+                    column = getattr(DonorsOverview, column_name)
+                    contains = getattr(column, "contains")
+                    conditions_all[-1].append(contains(part, autoescape=True))
+        filter_ = db.and_(*[db.or_(*conditions) for conditions in conditions_all])
+    else:
+        filter_ = True
+
+    # ORDER BY part
+    order_by_column_id = int(params["order[0][column]"])
+    order_by_column_name = list(COLUMN_NAMES.keys())[order_by_column_id]
+    order_by_direction = params["order[0][dir]"]
+    if hasattr(DonorsOverview, order_by_column_name):
+        order_by_column = getattr(DonorsOverview, order_by_column_name)
+        order_by = (getattr(order_by_column, order_by_direction)(),)
+    elif order_by_column_name == "donations":
+        order_by_column_name = "donation_count_total"
+        order_by_column = getattr(DonorsOverview, order_by_column_name)
+        order_by = (getattr(order_by_column, order_by_direction)(),)
+    elif order_by_column_name == "last_award":
+        order_by = []
+        for medal in reversed(all_medals):
+            order_by_column = getattr(DonorsOverview, "awarded_medal_" + medal.slug)
+            order_by.append(getattr(order_by_column, order_by_direction)())
+
+    # LIMIT, OFFSET
+    limit = int(params["length"])
+    offset = int(params["start"])
+
+    # Final query without limits to see how many records we have after filtering
+    # this number is important for pagination
+    filtered_records_count = (
+        DonorsOverview.query.filter(filter_).order_by(*order_by).count()
+    )
+    # Final query
+    overview = (
+        DonorsOverview.query.filter(filter_)
+        .order_by(*order_by)
+        .limit(limit)
+        .offset(offset)
+        .all()
+    )
+
+    # Data processing
+    final_list = []
+    for donor in overview:
+        # All standard attributes
+        donor_dict = {name: getattr(donor, name, None) for name in COLUMN_NAMES.keys()}
+        # Highest awarded medal
+        for medal in all_medals:
+            if getattr(donor, "awarded_medal_" + medal.slug):
+                donor_dict["last_award"] = medal.title
+                break
+            else:
+                donor_dict["last_award"] = "Žádné"
+        # Dict with all donations which we use on frontend
+        # to generate tooltip
+        donor_dict["donations"] = {
+            dc.slug: {
+                "count": getattr(donor, "donation_count_" + dc.slug),
+                "name": dc.title,
+            }
+            for dc in all_donation_centers
+        }
+        donor_dict["donations"]["total"] = donor.donation_count_total
+        final_list.append(donor_dict)
+    return jsonify(
+        {
+            "data": final_list,
+            "recordsTotal": all_records_count,
+            "recordsFiltered": filtered_records_count,
+        }
+    )
 
 
 @blueprint.get("/detail/<rc>")
