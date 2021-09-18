b79c3a708ebec23877ed28abf1259b455063c0be
Lumir Balhar <lbalhar@redhat.com>
2021-09-18 11:20:02 +0200

    Přidej testy pro capitalize v šablonách

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,9 +1,11 @@
 import pytest
+from flask import url_for
 from wtforms.validators import ValidationError
 
+from registry.donor.models import DonorsOverview
 from registry.utils import NumericValidator
 
-from .helpers import FakeForm
+from .helpers import FakeForm, login
 
 
 class TestNumericValidator:
@@ -95,3 +97,51 @@ class TestCapitalizer:
         capitalize = testapp.app.jinja_env.filters["capitalize"]
         output = capitalize(input)
         assert output == expected
+
+    def test_capitalize_in_templates(self, user, testapp, db):
+        rodne_cislo = "1234567890"
+        do = DonorsOverview(
+            rodne_cislo=rodne_cislo,
+            first_name="KAREL",
+            last_name="VOMÁČKA",
+            address="LIPOVÁ 33",
+            city="OSTRAVA",
+            postal_code="71600",
+            kod_pojistovny="213",
+            donation_count_fm=50,
+            donation_count_fm_bubenik=50,
+            donation_count_trinec=50,
+            donation_count_mp=50,
+            donation_count_manual=50,
+            donation_count_total=250,
+            awarded_medal_br=False,
+            awarded_medal_st=False,
+            awarded_medal_zl=False,
+            awarded_medal_kr3=False,
+            awarded_medal_kr2=False,
+            awarded_medal_kr1=False,
+            awarded_medal_plk=False,
+        )
+
+        db.session.add(do)
+        db.session.commit()
+
+        login(user, testapp)
+
+        pages = (
+            ("donor.detail", {"rc": rodne_cislo}),
+            ("donor.award_prep", {"medal_slug": "br"}),
+            ("donor.render_award_document", {"rc": rodne_cislo, "medal_slug": "br"}),
+        )
+
+        for page, params in pages:
+            res = testapp.get(url_for(page, **params))
+            assert "KAREL" not in res
+            assert "VOMÁČKA" not in res
+            assert "Karel" in res
+            assert "Vomáčka" in res
+            if "award_document" not in page:
+                assert "LIPOVÁ 33" not in res
+                assert "OSTRAVA" not in res
+                assert "Lipová 33" in res
+                assert "Ostrava" in res
