9bfed763bbd026e2169c5675d3061c4bc29e48e1
Lumir Balhar <lbalhar@redhat.com>
2022-06-07 20:13:13 +0200

    Use sqlite icu for case insensitive LIKE

--- registry/extensions.py
+++ registry/extensions.py
@@ -5,6 +5,7 @@ in app.py.
 import locale
 from sqlite3 import Connection as SQLite3Connection
 
+import sqlite_icu
 from flask_bcrypt import Bcrypt
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_login import LoginManager
@@ -28,6 +29,11 @@ def _set_sqlite_params(dbapi_connection, connection_record):
         # Create collation for proper sorting
         locale.setlocale(locale.LC_ALL, "cs_CZ.utf8")
         dbapi_connection.create_collation("czech", locale.strcoll)
+
+        # Load SQLite ICU extension for case-insensitive LIKE
+        dbapi_connection.enable_load_extension(True)
+        dbapi_connection.load_extension(sqlite_icu.extension_path().replace(".so", ""))
+        dbapi_connection.enable_load_extension(False)
 
         # Activate foreign keys
         cursor = dbapi_connection.cursor()
